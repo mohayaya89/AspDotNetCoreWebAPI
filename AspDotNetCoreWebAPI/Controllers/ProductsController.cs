0d6a32d31682edc4dced340979c2cae530964b81
mohayaya89 <moha.john89@gmail.com>
2025-12-25 15:27:08 -0500

    Initial commit

---
+++ AspDotNetCoreWebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using AspDotNetCoreWebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspDotNetCoreWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ShopContext _context;
+
+        public ProductsController(ShopContext context) 
+        {
+            _context = context;
+            _context.Database.EnsureCreated();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAllProducts()
+        {
+            return Ok(await _context.Products.ToArrayAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
---
+++ AspDotNetCoreWebAPI/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace AspDotNetCoreWebAPI.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public virtual List<Product>? Products { get; set; }
+
+    }
+}
